81bd72117eaa58c1d22f9342e71b0d59cdddd69d
mariabrenneisen <maria_e_brenneisen@estudante.sc.senai.br>
2022-10-03 09:14:13 -0300

    Cadastro

--- inserir.php
+++ inserir.php
@@ -1,9 +1,16 @@
 <?php
     include("conexao.php");
 
+    $email = $_POST["nome"];
+    $senha = $_POST["sobrenome"];
+    $email = $_POST["cpf"];
+    $senha = $_POST["data_nascimento"];
     $email = $_POST["email"];
     $senha = MD5($_POST["senha"]);
+    $email = $_POST["login_usuario"];
+    
     $comando = $pdo -> prepare("INSERT INTO pessoa (nome, sobrenome, cpf, data_nascimento,email, senha, login_usuario) VALUES(:nome,:sobrenome,:cpf,:data_nascimento,:email,:senha,:login_usuario)");
+    
     $comando->bindValue(":nome",$nome);                                     
     $comando->bindValue(":sobrenome",$sobrenome); 
     $comando->bindValue(":cpf",$cpf);                                     
