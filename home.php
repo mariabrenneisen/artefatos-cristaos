71501f8c98e7a7c4fa54ade8c629063073501db0
cadu2302 <carlos_el_goncalves@estudante.sc.senai.br>
2022-10-03 11:34:10 -0300

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
     session_start();
     // Verifique se o usuário está logado, se não, redirecione-o para uma página de login
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: login.php");
+        //header("location: login.php");
         exit;
     }
 ?>
