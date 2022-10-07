849f9c22b2e5f4668c82edf8fa16c39e0703f651
mariabrenneisen <maria_e_brenneisen@estudante.sc.senai.br>
2022-10-07 10:55:11 -0300

    .
    .

--- inserir_resenhas.php
+++ inserir_resenhas.php
@@ -5,11 +5,13 @@
     $autor = $_POST["autor"];
     $resenha = $_POST["resenha"];
 
-    $comando = $pdo->prepare("INSERT INTO resenha (tituloLivro, autor, resenha) VALUES(:tituloLivro,:autor,:resenha)");
+    $comando = $pdo->prepare("INSERT INTO resenha (tituloLivro, autor, resenha,fk_usuario_resenha) VALUES(:tituloLivro,:autor,:resenha,:fk_usuario_resenha)");
     
+
     $comando->bindValue(":tituloLivro",$tituloLivro);                                     
     $comando->bindValue(":autor",$autor); 
     $comando->bindValue(":resenha",$resenha);
+    $comando->bindValue(":fk_usuario_resenha",$_SESSION['codUsuario']);
     
     $comando->execute();     
     
--- login.php
+++ login.php
@@ -8,6 +8,7 @@
         $comando->bindValue(":email", $email);
         $comando->execute();
 
+
         if($comando->rowCount()==1)
         {
             $resultado = $comando->fetch();
